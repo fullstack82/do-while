9abe8f86722a18a15c7bd6c9d42238fa9b1e11fe
fullstack82 <fullstack8210@gmail.com>
2021-01-29 01:01:26 +0100

    do-while

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+// Bucle Do-while la condicion se evalua al final del bloque de instrucciones, es util cuando queremos que se ejecute una vez y luego comprobar algo.
+
+
+
+/*do{
+
+    // Bloque de instrucciones
+
+
+}while(condicion);   //Evaluamos la condición
+
+*/
+
+
+$edad = 17;
+$contador = 1;
+
+do {
+    echo "Tienes acceso al local privado $contador"."<br>"; 
+    $contador++;
+} while ($edad >= 18 && $contador <= 100);  // Si esta condición se cumple el bucle se vuelve a ejecutar, si no se cumple solo se ejecutara el primer bucle y parara;
